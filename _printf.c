2938851d8f497c111c5b3e26c447284278e909d4
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-10-18 02:49:40 -0700

    Task0

---
+++ _printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: specify the text format
+ *
+ * Return: number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int (*func)(va_list);
+	int i = 0;
+	int len_arg = 0;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			return (-1);
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+			len_arg++;
+			i += 2;
+			continue;
+		}
+		else if (format[i] == '%')
+		{
+			func = get_op_func(&format[i]);
+			if (func != NULL)
+			{
+				len_arg += func(ap);
+				i += 2;
+				continue;
+			}
+			else
+			{
+				if (format[i] == '%' && format[i + 1] == '\0')
+				{
+					len_arg++;
+					_putchar(format[i]);
+				}
+				else
+				{
+					len_arg++;
+					_putchar(format[i]);
+				}
+			}
+		}
+		/* if format[i] == \n */
+		else if (format[i] == 92 && format[i + 1] == 110)
+		{
+			_putchar('\n');
+			break;
+		}
+		else if (format[i] != '\0')
+		{
+			_putchar(format[i]);
+			len_arg++;
+		}
+		i++;
+	}
+	va_end(ap);
+	return (len_arg);
+}
---
+++ main.h
@@ -0,0 +1,57 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+/* PROTOTYPES */
+
+/* MAIN FUNCTION */
+int _printf(const char *format, ...);
+int (*get_op_func(const char *format))(va_list);
+
+/* PRINT FUNCTION TYPES */
+
+/* PRINT TEXT */
+int print_char(va_list);
+int print_string(va_list);
+int print_rev_str(va_list);
+int print_rot13(va_list);
+/* PRINT NUMBERS */
+int print_decimal(va_list);
+
+/* AUX FUNCTION */
+char *int_to_arg(int i, char *strout, int base);
+int _putchar(char c);
+void _putnum(int n);
+int _strlen(char *s);
+int _intlen(int i);
+
+/* STRUCTURES */
+
+/**
+ *  * struct functions - hashmap structure
+ *   * @type: argument type specified by %
+ *    * @function: function that perfome with the specified argument
+ *     */
+typedef struct functions
+{
+		char *type;
+			int (*function)(va_list);
+} functions_t;
+
+static const functions_t types[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{"d", print_decimal},
+	{"i", print_decimal},
+	{"r", print_rev_str},
+	{"R", print_rot13},
+	{NULL, NULL}
+};
+
+#endif /* MAIN_C */
